b650dbdbbd55684cace8111bbbbc61c8c5ccf650
arnemergan <mergan.arne@hotmail.com>
2020-06-05 22:28:41 +0200

    clean up code

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const path = require("path");
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + '/dist'));
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 app.get('*', function(req,res){
 res.sendFile(path.join(__dirname, 'src', 'index.html'))
 });
